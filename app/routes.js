141d5a19ea1a8bbb63bef0b2205774986961362d
Colin Oakley <colin.oakley@engineering.digital.dwp.gov.uk>
2022-07-22 15:40:58 +0100

    adding lookup

--- app/routes.js
+++ app/routes.js
@@ -5,12 +5,24 @@ const _ = require('underscore')
 
 const { getData } = require('../app/data')
 
-const data = getData();
+const data = getData()
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
 
 router.get('/', (req, res) => {
     const sortedData = _.sortBy(data, 'termName')
-    const groupedData = _.groupBy(sortedData, 'initial');
+    const groupedData = _.groupBy(sortedData, 'initial')
+    console.log(groupedData)
     res.render('index.html', { groupedData });
 })
+
+router.get('/:termName', (req, res) => {
+    const { termName } = req.params
+    const item = _.findWhere(data, {termName: capitalizeFirstLetter(termName.replace('_', ' '))})
+    res.render('terms.html', { item })
+})
+
 
 module.exports = router
