41f96caf692b16d2fbd9a3dfc445efe413ca06c2
danhowarthdwp <danhowarth.digitaldwp@gmail.com>
2022-07-22 17:40:52 +0100

    Removed gov logo from header

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Glossary tech spike',
+  serviceName: 'DWP Health data glossary tech spike',
 
   // Default port that prototype runs on
   port: '3000',
